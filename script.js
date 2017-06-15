c6ae41dd808c5e5e52e82904e7ab983504bab0a9
Samuel  Escapa Otani <samuel.escapa@pc1274.local>
2017-06-15 11:40:32 +0200

    improvement accross and update on content

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 $(document).ready(function() {
-  // $(".enter span").click(function() {
+  $(".enter span").click(function() {
     $(".text").toggleClass("second");
     $(".screen.one").toggleClass("second");
-  // })
+  });
+  $(".goback").click(function() {
+    $(".text").toggleClass("second");
+    $(".screen.one").toggleClass("second");
+  })
+  $(".image").click(function() {
+    $(".center").hide();
+    $(".image").toggleClass("exit");
+    $(".descriptions").show();
+    $(".description").removeClass("active");
+    $(".description." + $(this).attr("id")).addClass("active");
+  });
+  $(".backbutton").click(function() {
+    $(".image").toggleClass("exit");
+    $(".descriptions").hide();
+    $(".center").show();
+  });
 });
